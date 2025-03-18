4e8b82b20ebac17c500b31cb89c109332f41371b
the-destructor <73207042+the-destructor@users.noreply.github.com>
2025-03-19 09:54:22 +1100

    lab 1 done

--- lab1.py
+++ lab1.py
@@ -8,18 +8,19 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a list named favorite_foods and add at least five different food items to it
-
+foods = ["bob", "bob2", "bob3", "bob4", "bob5"]
 
 # Change the third item in the list to a different food
-
+foods[2] = "newbob"
 
 # Print out only the first and fourth items in the list
-
+print(foods[0], foods[3])
 
 # Ask the user for a food item
-
+fooditem = input("What is your fav food?: ")
 
 # Append the user's food item to the list
-
+foods.append(fooditem)
 
 # Print out the entire list of food items
+print(foods)
